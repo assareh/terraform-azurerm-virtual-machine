eadb3342ca0e4a00164f27659b856e518285f6df
assareh <assareh@hashicorp.com>
2021-06-28 09:31:31 -0700

    initial commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "password" {
+  description = "The admin password for the virtual machine."
+}
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "username" {
+  description = "The admin username for the virtual machine."
+}
